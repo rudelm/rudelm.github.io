db6abac3fe5e17ef05eeec7c43dab4dc40fa96c2
Markus Rudel <markus@rudel.email>
2022-01-02 21:38:46 +0100

    Added clean script for images

---
+++ clean-image-versions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import os
+import re
+
+content_dir = 'static/wp-content/uploads'
+
+def delete_file(file_entry):
+    if os.path.exists(file_entry):
+        os.remove(file_entry)
+    else:
+        print("Can not delete {} as it doesn't exists".format(file_entry))
+
+for root, dirs, files in os.walk(content_dir):
+    for file in files:
+        result = re.search(r"(.*)-(\d{3}x\d{3}).*", file)
+        if result:
+            delete_file(os.path.join(root, file))
